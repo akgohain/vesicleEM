4862ddbaa0c5fe1569367e615f746a040a313e29
Michael Lin <michael.lin2k21@gmail.com>
2025-06-13 15:49:04 -0400

    fix 7-13 mask again

--- ves_seg/sample/ng.py
+++ ves_seg/sample/ng.py
@@ -27,20 +27,16 @@ data_dir = 'sample'
 vol_name = '7-13'
 
 clahe = readvol(os.path.join(data_dir, vol_name+"_clahe.h5"))
-mask = readvol(os.path.join(data_dir, vol_name+"_mask.h5")).astype(np.uint8)
+mask = readvol(os.path.join(data_dir, vol_name+"_mask.h5"))
 gt = readvol(os.path.join(data_dir, vol_name+"_ves.h5"))
 pred = readvol(os.path.join(data_dir, vol_name+"_pred.h5"))
 
-mask_orig = mask
-for i in range(2):
-    mask = morphology.binary_dilation(mask, morphology.ball(radius=2))
-gt= gt * mask
 pred = pred * mask
 
 with viewer.txn() as s:
     s.layers.append(name='clahe',layer=ng_layer(clahe, res=[8, 8, 30], tt='image'))
     s.layers.append(name='gt',layer=ng_layer(gt, res=[8, 8, 30], tt='segmentation'))
-    s.layers.append(name='mask',layer=ng_layer(mask_orig, res=[8, 8, 30]))
+    s.layers.append(name='mask',layer=ng_layer(mask, res=[8, 8, 30]))
     s.layers.append(name='pred',layer=ng_layer(pred, res=[8, 8, 30], tt='segmentation'))
 
 print(viewer)
